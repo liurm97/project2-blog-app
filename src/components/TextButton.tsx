f4ab7aeaa5a7579d293445929e0ef5a6b5dc7d84
bobby.liu <fellowe5@hotmail.com>
2024-03-31 11:32:51 +0800

    corrected TS warnings/errors

--- src/components/TextButton.tsx
+++ src/components/TextButton.tsx
@@ -1,5 +1,5 @@
 import cn from "../utils";
-import { EditorBubbleItem, useEditor } from "novel";
+import { EditorBubbleItem, useEditor, EditorInstance } from "novel";
 import {
   BoldIcon,
   ItalicIcon,
@@ -17,38 +17,38 @@ export type SelectorItem = {
   isActive: (editor: ReturnType<typeof useEditor>["editor"]) => boolean;
 };
 
-export const TextButtons = ({ editor }) => {
+export const TextButtons = ({ editor }: { editor: any }) => {
   // const { editor } = useEditor();
   if (!editor) return null;
   const items: SelectorItem[] = [
     {
       name: "bold",
-      isActive: (editor) => editor.isActive("bold"),
-      command: (editor) => editor.chain().focus().toggleBold().run(),
+      isActive: (editor) => editor!.isActive("bold"),
+      command: (editor) => editor!.chain().focus().toggleBold().run(),
       icon: BoldIcon,
     },
     {
       name: "italic",
-      isActive: (editor) => editor.isActive("italic"),
-      command: (editor) => editor.chain().focus().toggleItalic().run(),
+      isActive: (editor) => editor!.isActive("italic"),
+      command: (editor) => editor!.chain().focus().toggleItalic().run(),
       icon: ItalicIcon,
     },
     {
       name: "underline",
-      isActive: (editor) => editor.isActive("underline"),
-      command: (editor) => editor.chain().focus().toggleUnderline().run(),
+      isActive: (editor) => editor!.isActive("underline"),
+      command: (editor) => editor!.chain().focus().toggleUnderline().run(),
       icon: UnderlineIcon,
     },
     {
       name: "strike",
-      isActive: (editor) => editor.isActive("strike"),
-      command: (editor) => editor.chain().focus().toggleStrike().run(),
+      isActive: (editor) => editor!.isActive("strike"),
+      command: (editor) => editor!.chain().focus().toggleStrike().run(),
       icon: StrikethroughIcon,
     },
     {
       name: "code",
-      isActive: (editor) => editor.isActive("code"),
-      command: (editor) => editor.chain().focus().toggleCode().run(),
+      isActive: (editor) => editor!.isActive("code"),
+      command: (editor) => editor!.chain().focus().toggleCode().run(),
       icon: CodeIcon,
     },
   ];
